b2ed8b25432a18abe016d7720a095cadb47887f9
mitch <mitch.c.dominici@gmail.com>
2024-12-19 22:49:29 -0600

    12 - comment out events export for now

--- .svelte-kit/generated/client/nodes/7.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/towns/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/towns/[slug]/+page.svelte";
