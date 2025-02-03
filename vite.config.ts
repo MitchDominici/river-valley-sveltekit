9e997542cb767e9a98b27eab81742bc05e6f719f
mitch <mitch.c.dominici@gmail.com>
2025-02-03 16:57:20 -0600

    23 - trying to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  // Add base URL when building for production
-  base: process.env.NODE_ENV === 'production' ? '/river-valley-sveltekit/' : '/'
 });
